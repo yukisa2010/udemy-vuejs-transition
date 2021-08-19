662a03a8194766e5d91a2b769cc6d57afbda89cf
yukisa2010 <yukisa2010@gmail.com>
2021-08-19 16:36:10 +0900

    add enter & leave class

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <transition 
+      enter-active-class="animated bounce"
+      leave-active-class="animated shake"
+      appear
+    >
+      <p v-if="show">hello</p>
+    </transition>
+    <transition name="slide" type="animation" appear>
+      <p v-show="show">bye</p>
+    </transition>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      show: true
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .main {
+    width: 70;
+    margin: auto;
+    padding-top: 5rem;
+    text-align: center;
+  }
+  /* --fade-- */
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 0.5s;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+  .fade-leave {
+    opacity: 1;
+  }
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  /* --slide-- */
+
+  .slide-enter,
+  .slide-leave-to {
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 0.5s;
+    transition: opacity 1s;
+  }
+  .slide-leave-active {
+    animation: slide-in 0.5s reverse;
+    transition: opacity 1s;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(100px)
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
 </style>
